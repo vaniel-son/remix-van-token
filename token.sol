ec5f04e5f7b61d3539dc4442fa45ac7cb49da661
"Vanielson" <"vanielson1000@gmail.com">
2022-06-20 11:14:14 -0400

    van token

---
+++ token.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.2;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/presets/ERC20PresetMinterPauser.sol";
+
+// POLYGON TEST NETWORK MUMBAI (CHAINLIST)
+// contract1 VAN1 token: 0xeea0E0351967C793B157792afDd7D0A2104B0B11
+// contract2 VAN2 token: 0x595ee94Ef08AA90940d674151907eE5Ad512ae8e
