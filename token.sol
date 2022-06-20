06c7f4e556a0f7aa06a4ea47b7a6dea56d1c6f66
"Vanielson" <"vanielson1000@gmail.com">
2022-06-20 13:53:05 -0400

    van token

--- token.sol
+++ token.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.6.2;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/presets/ERC20PresetMinterPauser.sol";
 
+// https://forum.openzeppelin.com/t/create-an-erc20-using-remix-without-writing-solidity/2908
+
 // POLYGON TEST NETWORK MUMBAI (CHAINLIST)
 // contract1 VAN1 token: 0xeea0E0351967C793B157792afDd7D0A2104B0B11
 // contract2 VAN2 token: 0x595ee94Ef08AA90940d674151907eE5Ad512ae8e
